05dcd62f6983db2ae3b5cd6817e5e809549ce0c7
Onurege <78586675+onure9e@users.noreply.github.com>
2021-06-19 13:31:43 +0300

    Add files via upload

---
+++ Onurege.js
@@ -0,0 +1,37 @@
+var Discord = require('discord.js'), data = new Map(), config = require("./Config.json")
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    } 
+  }
+config.tokens.forEach(token=>{
+    var client = new Discord.Client()
+    client.login(token).catch(()=>{
+        console.log("Bir Tokende Hata bulundu & Geçersiz")
+    })
+
+    setInterval(() => {
+        var randomChannel = Math.floor(Math.random()*(config.j4jChannels.length-0+0)+0);
+        randomChannel = config.j4jChannels[randomChannel]
+        var channel = client.channels.cache.find(channel=>channel.id === randomChannel)
+        channel.send("j4j dm")
+    }, 15000);
+    client.on("message",async message=>{
+        if(message.channel.type == "dm"){
+            if(data.has(`${message.author.id}.j4j`)) return;
+            if(message.content.includes("discord.gg")){
+                data.set(`${message.author.id}.j4j`,true)
+                sleep(1500)
+                message.channel.send(config.serverInviteLink)
+                message.channel.startTyping()
+                sleep(600)
+                message.channel.send("Done")
+                message.channel.stopTyping()
+                client.channels.cache.get(config.j4jLog).send(`${message.author.tag} | ${message.author.id} | ${message.author}\nj4j ile davet edildi`)
+            }
+        }
+    })
+})
---
+++ README.md
@@ -0,0 +1,12 @@
+<img align="right" height="150rem" alt="GIF" src="https://user-images.githubusercontent.com/78586675/121921369-9cabb880-cd41-11eb-8814-0da7526ab152.gif"/>
+
+
+## Merhabalar Sizin için derlediğim **Join4Join Bot** Altyapısına Hoşgeldiniz.
+
+## <i> Kurulum </i>
+
+**Altyapıyı yükledikten sonra terminal bölümüne ```npm i``` kodunu yazınız bu kod botun çalışması için gerekli modülleri (discord.js) otomatik olarak yükler sonrasında ise ```Config.json``` adlı dosyayı doldurmanız gerekmektedir.**
+
+## <i> Bilgilendirme </i>
+
+**Çalınması vb durumlarda gerekli işlemler başlatılacaktır.**
